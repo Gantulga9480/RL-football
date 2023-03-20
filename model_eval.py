d2ca4adc7bcef0d65c2b0ceff94206282a366e95
Gantulga <limited.tulgaa@gmail.com>
2023-03-21 01:57:22 +0800

    changed some utility scripts

--- model_eval.py
+++ model_eval.py
@@ -1,62 +1,53 @@
 import os
 import torch
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from single_agent_env import SinglePlayerFootball
 from RL import DeepQNetworkAgent, ActorCriticAgent
+
+font = {'size': 18}
+matplotlib.rc('font', **font)
 
 env = SinglePlayerFootball(title="Model evaluation")
 agent = DeepQNetworkAgent(None, None)
 agent.train = False
 
-# base = "models/random_ball"
-# paths = []
-# for root, dirs, files in os.walk(base):
-#     for file in files:
-#         if file.endswith(".pt"):
-#             paths.append(f"{root}/{file}")
-
-paths = [
-    "models/random_ball/1/-0.01_every_to_1 iteration 2_31275_0.99.pt",
-    "models/random_ball/2/random_ball_every_-1_goal_300_transfer_14705_299.pt",
-    "models/random_ball/3/random_ball_normalized_5019_0.99.pt",
-    "models/random_ball/4/random_ball_normalized_transfer_26914_299.pt"
-]
+base = r"models\random_ball"
+paths = []
+for root, dirs, files in os.walk(base):
+    for file in files:
+        if file.endswith(".pt"):
+            paths.append(f"{root}\\{file}")
+paths.sort()
 
 sim_scores = []
-
 for path in paths:
     agent.model = torch.jit.load(path, map_location="cpu")
-
     ep_rewards = []
-    for _ in range(1000):
+    for _ in range(10):
         rewards = []
         state = env.reset(ball_random=True)
         while not env.loop_once():
             state, reward, done = env.step(agent.policy(state))
             rewards.append(reward)
         ep_rewards.append(np.sum(rewards))
-
     sim_scores.append(np.mean(ep_rewards))
 
 best_model = paths[np.argmax(sim_scores)]
-print(best_model)
+print(f"{best_model=}")
 print(sim_scores)
-plt.bar(paths, sim_scores)
+
+fig, ax = plt.subplots(1)
+if len(sim_scores) > 1:
+    plt.title("Average score of 1000 episodes")
+    ax.set_xlabel("Trainig iterations")
+    ax.set_ylabel("Score")
+    ax.bar([range(len(sim_scores))], sim_scores)
+    ax.set_xticks([])
+else:
+    plt.title("Model performance over 1000 episodes")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Reward")
+    ax.plot(ep_rewards)
 plt.show()
-
-
-# agent.model = torch.jit.load(f"models/random_ball/first/-0.01_every_to_1 iteration 2_31275_0.99.pt", map_location="cpu")
-
-# ep_rewards = []
-# for _ in range(100):
-#     rewards = []
-#     state = env.reset(ball_random=True)
-#     while not env.loop_once():
-#         state, reward, done = env.step(agent.policy(state))
-#         rewards.append(reward)
-#     ep_rewards.append(np.sum(rewards))
-
-# print(np.mean(ep_rewards))
-# plt.plot(ep_rewards)
-# plt.show()
--- plot.py
+++ plot.py
@@ -1,30 +1,72 @@
-import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+import matplotlib.pylab as plt
 import argparse
 
+font = {'weight': 'normal', 'size': 18}
+matplotlib.rc('font', **font)
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', type=str, default='')
+parser.add_argument('rewards', nargs='+')
+parser.add_argument('--width', type=int, default=100)
+parser.add_argument("--average", action="store_true")
+parser.add_argument('--save', action="store_true")
 args = parser.parse_args()
-scores = []
-if args.file:
-    with open(args.file, 'r') as f:
-        data = f.readlines()
-        scores = [float(item) for item in data]
-else:
-    quit()
 
-scores_sum_small = []
-scores_sum_big = []
 
-window_size = 50
-for i in range(len(scores) - window_size):
-    scores_sum_small.append(sum(scores[i:i + window_size]) / window_size)
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w
 
-window_size = 500
-for i in range(len(scores) - window_size):
-    scores_sum_big.append(sum(scores[i:i + window_size]) / window_size)
 
-fig, ax1 = plt.subplots()
-ax1.plot(scores_sum_small, alpha=0.4, c='r', linewidth='10')
-ax2 = ax1.twinx()
-ax2.plot(scores_sum_big, c='b', linewidth='4')
-plt.show()
+if not args.average:
+    rewards_list = []
+    file_names = []
+    for file in args.rewards:
+        try:
+            with open(file, 'r') as f:
+                lines = f.readlines()
+                rewards_list.append(np.array([float(item.strip()) for item in lines]))
+                file_names.append(file.split(".")[0])
+        except IsADirectoryError:
+            continue
+
+    avgs = []
+    for rewards in rewards_list:
+        avgs.append(moving_average(rewards, args.width))
+
+    fig1, ax1 = plt.subplots()
+    for i, r in enumerate(avgs):
+        ax1.plot(r, label=file_names[i], linewidth="5")
+    plt.title("Training result")
+    ax1.legend()
+    ax1.set_xlabel("Episode")
+    ax1.set_ylabel("Reward")
+    plt.show()
+
+elif args.average:
+    rewards = []
+    file_names = []
+    for file in args.rewards:
+        try:
+            with open(file, 'r') as f:
+                lines = f.readlines()
+                data = [float(item.strip()) for item in lines]
+                rewards.append(np.array(data))
+                file_names.append(file.split(".")[0])
+        except IsADirectoryError:
+            continue
+    np_r = np.array(rewards)
+    np_r = np_r.mean(axis=0)
+    avg = moving_average(np_r, args.width)
+
+    fig1, ax1 = plt.subplots()
+    ax1.plot(avg, label="Average", linewidth="5")
+    ax1.legend()
+    ax1.set_xlabel("Episode")
+    ax1.set_ylabel("Reward")
+    plt.title("Average training curve")
+    plt.show()
+
+    if args.save:
+        with open(f'{file_names[0]}', 'w') as f:
+            f.writelines([str(item) + '\n' for item in avg])
